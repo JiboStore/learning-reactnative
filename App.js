bfc087f7fef3e8e598ce09e970d80f129c3f68a3
Hakim Hauston <hakim.hauston@gmail.com>
2017-08-17 17:58:06 +0800

    [mangafun] react native: added image

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { AppRegistry, Image } from 'react-native';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
+    let pic = {
+      uri: 'https://instagram.fsin1-1.fna.fbcdn.net/t51.2885-15/e35/15625222_1297217123678135_6944271839078121472_n.jpg'
+    };
     return (
       <View style={styles.container}>
+        <Image source={pic} style={{width: 320, height: 320}}/>
         <Text>Open up App.js to start working on your app!</Text>
         <Text>Changes you make will automatically reload.</Text>
         <Text>Shake your phone to open the developer menu.</Text>
